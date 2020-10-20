96f981b2d36e646181cdccf4a1729f3d571b1be8
Darya-sha <70708813+Darya-sha@users.noreply.github.com>
2020-10-20 22:00:27 +0300

    fix: variable

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,7 +4,7 @@ const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 const  HALF_LIFE_COEFFICIENT = 0.693;
 module.exports = function dateSample(sampleActivity) {
-  let sampleNumber = Number.parseFloat(sampleActivity);
+  const sampleNumber = Number.parseFloat(sampleActivity);
   if (typeof (sampleActivity) === 'string' && sampleNumber > 0 && sampleNumber <= MODERN_ACTIVITY) {
     return Math.ceil(Math.log(MODERN_ACTIVITY / sampleNumber) * (HALF_LIFE_PERIOD / HALF_LIFE_COEFFICIENT));
   }
